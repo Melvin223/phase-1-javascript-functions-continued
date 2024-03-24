0b21ee5707ba27d7824046d317d8f9e84b633df6
kym <melvinkimani81@gmail.com>
2024-03-24 04:23:56 +0300

    creating myfiles.md

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(){
+    console.log('This Saturday, I want to roller-skate!');
+}
+
+const mondayWork = function(){
+    console.log("This Monday, I will go to the office");
+}
+function wrapAdjective(_flair = '*'){
+    return function(_adjective = 'special'){
+        return 'You are *a hard worker*!';
+    };
+}
